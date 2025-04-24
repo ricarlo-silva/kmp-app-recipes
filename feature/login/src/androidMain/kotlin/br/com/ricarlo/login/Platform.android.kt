99c5dd99c63143b97bdcadc67a54c6b4a097c5cc
Ricarlo Silva <ricarlosilv@gmail.com>
2025-04-24 00:58:35 -0300

    feat: add login module

---
+++ feature/login/src/androidMain/kotlin/br/com/ricarlo/login/Platform.android.kt
@@ -0,0 +1,3 @@
+package br.com.ricarlo.login
+
+actual fun platform() = "Android"
--- feature/login/src/androidTest/kotlin/br/com/ricarlo/login/ExampleInstrumentedTest.kt
+++ feature/login/src/androidTest/kotlin/br/com/ricarlo/login/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package br.com.ricarlo.network
+package br.com.ricarlo.login
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.com.ricarlo.network.test", appContext.packageName)
+        assertEquals("br.com.ricarlo.login.test", appContext.packageName)
     }
 }
