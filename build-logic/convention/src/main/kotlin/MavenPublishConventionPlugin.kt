c806d2cda474856793f4fc89bc859ed8d66c00eb
Ricarlo Silva <ricarlosilv@gmail.com>
2025-04-27 16:14:14 -0300

    feat: config maven-publish plugin

--- build-logic/convention/src/main/kotlin/MavenPublishConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/MavenPublishConventionPlugin.kt
@@ -15,38 +15,36 @@ class MavenPublishConventionPlugin : Plugin<Project> {
             version = rootProject.file("./config/VERSION_NAME").readText().trim()
             group = "com.ricarlo"
 
-            afterEvaluate {
-                extensions.getByType(PublishingExtension::class.java).apply {
-                    publications.withType(MavenPublication::class.java).all {
-                        pom {
-                            name.set("${project.name.capitalized()} - CMP library")
-                            description.set("${project.name.capitalized()} - Compose Multiplatform library")
-                            url.set("https://github.com/ricarlo-silva/kmp-app-recipes")
+            extensions.getByType(PublishingExtension::class.java).apply {
+                publications.withType(MavenPublication::class.java).all {
+                    pom {
+                        name.set("${project.name.capitalized()} - CMP library")
+                        description.set("${project.name.capitalized()} - Compose Multiplatform library")
+                        url.set("https://github.com/ricarlo-silva/kmp-app-recipes")
 
-                            licenses {
-                                license {
-                                    name.set("MIT")
-                                    url.set("https://opensource.org/licenses/MIT")
-                                }
+                        licenses {
+                            license {
+                                name.set("MIT")
+                                url.set("https://opensource.org/licenses/MIT")
                             }
+                        }
 
-                            developers {
-                                developer {
-                                    id.set("ricarlo-silva")
-                                    name.set("Ricarlo Silva")
-                                    email.set("ricarlosilv@gmail.com")
-                                }
+                        developers {
+                            developer {
+                                id.set("ricarlo-silva")
+                                name.set("Ricarlo Silva")
+                                email.set("ricarlosilv@gmail.com")
                             }
                         }
                     }
-                    repositories {
-                        maven {
-                            name = "GitHubPackages"
-                            url = uri("https://maven.pkg.github.com/ricarlo-silva/kmp-app-recipes")
-                            credentials {
-                                username = System.getenv("GITHUB_ACTOR")
-                                password = System.getenv("GITHUB_TOKEN")
-                            }
+                }
+                repositories {
+                    maven {
+                        name = "GitHubPackages"
+                        url = uri("https://maven.pkg.github.com/ricarlo-silva/kmp-app-recipes")
+                        credentials {
+                            username = System.getenv("GITHUB_ACTOR")
+                            password = System.getenv("GITHUB_TOKEN")
                         }
                     }
                 }
